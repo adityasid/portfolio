89839f5e3d22f12703332d4200636528a48f4a24
Aditya <adityasid786@gmail.com>
2024-01-14 22:08:01 +0530

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-83YQNXN9EC"
+      />
+      <Script id="google-analytics">
+        {`
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+       
+         gtag('config', 'G-83YQNXN9EC');
+        `}
+      </Script>
     </html>
   );
 }
